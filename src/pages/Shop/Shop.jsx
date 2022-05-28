6eaa052cdad537c923ecce21c1417e5dc638ac0e
Safia Jotey <safiaahmed1786@gmail.com>
2022-05-28 19:36:25 +0600

    get products from data base

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -10,7 +10,7 @@ const Shop = () => {
   const [displaProducts, setDisplayProducts] = useState([]);
 
   useEffect(() => {
-    fetch('products.json')
+    fetch('http://localhost:5000/products')
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
